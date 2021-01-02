4f1e8027320e350f99ed5a53c127c41a88ea6a41
zmzfpc <2286992967@qq.com>
2021-01-02 17:17:24 +0800

    V1.0TEST

---
+++ mysql.py
@@ -0,0 +1,23 @@
+from flask import Flask,jsonify
+import pymysql
+
+db = pymysql.connect('8.133.173.118:13306','root','moreonenight24680no','datawarehouse')# 记得修改数据库地址
+cursor = db.cursor()
+
+app = Flask(__name__)
+
+@app.route('/<id>',methods=['GET'])
+def mysqlConnectionTest(id):
+    sql = 'select 名字 from 影片'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    names = []
+    for row in results:
+        name = row[0]
+        names.append(name)
+        if row == id:break
+    return names
+    
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=5000,debug=True)
