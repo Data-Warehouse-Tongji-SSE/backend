15366ebdd934a30733aaa3dfcf0ba4dfda092a4c
zmzfpc <2286992967@qq.com>
2021-01-02 18:17:12 +0800

    venv

--- mysql.py
+++ mysql.py
@@ -1,22 +1,33 @@
 from flask import Flask,jsonify
+from flask.json import tojson_filter
 import pymysql
 
-db = pymysql.connect('8.133.173.118:13306','root','moreonenight24680no','datawarehouse')# 记得修改数据库地址
+db = pymysql.connect(
+    host='8.133.173.118',
+    port=13306,
+    user='root',
+    passwd='moreonenight24680no',
+    db='datawarehouse',
+    )# 记得修改数据库地址
 cursor = db.cursor()
 
 app = Flask(__name__)
 
 @app.route('/<id>',methods=['GET'])
 def mysqlConnectionTest(id):
-    sql = 'select 名字 from 影片'
+    print(id)
+    sql = 'select movie_id,movie_title from movies'
     cursor.execute(sql)
     results = cursor.fetchall()
     names = []
+    title="1321"
     for row in results:
-        name = row[0]
-        names.append(name)
-        if row == id:break
-    return names
+        movie_id = row[0]
+        if movie_id == id:
+            title=row[1]
+            break  
+    print(title)
+    return title
     
 
 if __name__ == '__main__':
